96d3271bd698dfe3325d809b7e36a206dac279de
Adithya Rachmat <adithyarachmat21@gmail.com>
2023-08-03 13:58:25 +0700

    tes1

--- app/Http/Controllers/IklanController.php
+++ app/Http/Controllers/IklanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use App\Models\Iklan;
 use App\Http\Requests\IklanRequest;
 use Illuminate\Http\Request;
@@ -26,14 +27,14 @@ class IklanController extends Controller
 
         $data['paging'] = new PaginationResource($iklan);
         $data['records'] = $iklan->items();
-        
+
         if ($request->wantsJson()) {
         return $this->success($data, 'get records data success');
     }
 
         return view('iklan', $data);
 
-       
+
     }
 
     /**
