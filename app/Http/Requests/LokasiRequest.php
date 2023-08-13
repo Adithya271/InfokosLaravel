105375f1414d12a0c0fcf06e4902d4632ff6c4d8
Adithya Rachmat <adithyarachmat21@gmail.com>
2023-08-13 13:16:37 +0700

    update api

--- app/Http/Requests/LokasiRequest.php
+++ app/Http/Requests/LokasiRequest.php
@@ -13,7 +13,7 @@ class LokasiRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/TransaksiRequest.php
+++ app/Http/Requests/TransaksiRequest.php
@@ -13,7 +13,7 @@ class TransaksiRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
