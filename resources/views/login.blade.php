bf3b030a9b6b0bc345b805375fa88e393d627638
Adithya Rachmat <adithyarachmat21@gmail.com>
2023-08-20 11:23:20 +0700

    ss

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -29,6 +29,9 @@
                             <div class="p-5">
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
+                                     <div class="sidebar-brand-icon">
+                                        <img src="{{ asset('assets/img/logo.jpg') }}" alt="Logo" width="100" height="90">
+                                    </div>
                                     <h3 class="h4 text-gray-700 mb-4">Silahkan Login Terlebih Dahulu</h3>
 
                                 </div>
