62864cec28bad1b4c0cb2fa16b403bdf4e41e07b
Adithya Rachmat <adithyarachmat21@gmail.com>
2023-08-20 11:09:53 +0700

    ss

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -32,13 +32,13 @@
         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Admin Infokos</div>
-            </a>
+        <a class="sidebar-brand d-flex align-items-center justify-content-center">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <img src="{{ asset('assets/img/logo_infokos.png') }}" alt="Logo" width="30" height="30">
+            </div>
+            <div class="sidebar-brand-text mx-3">Admin Infokos</div>
+        </a>
+
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
