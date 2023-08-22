395b523475937114c1c4659e2f0fd1ffd012ccbc
Adithya Rachmat <adithyarachmat21@gmail.com>
2023-08-22 22:56:41 +0700

    asas

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -89,10 +89,9 @@
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
-                    <button class="btn btn-link btn-sm" id="toggleSidebar">
-                        <i class="fas fa-bars"></i>
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
                     </button>
-
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
