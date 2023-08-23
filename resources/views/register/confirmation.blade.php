9a54a8854e911f1013521acc9234d130f815faba
Adithya Rachmat <adithyarachmat21@gmail.com>
2023-08-24 01:54:17 +0700

    zxc

--- resources/views/register/confirmation.blade.php
+++ resources/views/register/confirmation.blade.php
@@ -1,12 +1,12 @@
-<x-mail::message>
-    # Introduction
-    Dear {{$user->nama}}, silahkan klik link dibawah ini untuk konfirmasi pendaftaran.
+@component('mail::message')
+# Introduction
+Dear {{ $user->nama }}, silahkan klik link dibawah ini untuk konfirmasi pendaftaran.
 
-    <x-mail::button :url="$url">
-        Konfirmasi
-    </x-mail::button>
+@component('mail::button', ['url' => $url])
+Konfirmasi
+@endcomponent
 
-    <hr>
-    Thanks,<br>
-    {{ config('app.name') }}
-</x-mail::message>
+<hr>
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
